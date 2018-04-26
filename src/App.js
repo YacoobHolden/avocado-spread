bc3cfb24301a51595bc836ae33fcfdfba371a767
Ted Chen <ted.chen@eroad.com>
2018-04-26 20:56:16 +1200

    Use mockstore sometimes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import Main from './components/Main';
 import './App.css';
 import { Provider } from 'mobx-react';
-import Store from './stores/store.mock';
+import ProdStore from './stores/store';
+import MockStore from './stores/store.mock';
+
+const env = window.location.hostname === 'localhost' ? 'local' : 'depot';
+console.log(env);
+const Store = env === 'local' ? MockStore : ProdStore;
 
 class App extends Component {
   render() {
     const store = new Store();
     store.openNotificationSocket();
     return (
-      <Provider store={store}>
+      <Provider store={store} >
         <Main />
       </Provider>
     );
